3ec08833640b33eb65305871be8475887252e9a7
MohammadxAnas <mohammadanasqadar@gmail.com>
2024-10-26 23:58:48 +0530

    fixing toolkit

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,26 @@ import {useState} from 'react';
 import './hq.css';
 
 function Home(props) {
+
+    let [selectedText, setSelectedText] = useState('');
+    const handleMouseUp = (event) => {
+      const textarea = event.target;
+      const stdtext = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+      setSelectedText(stdtext);
+      console.log(selectedText);
+      const toolkit = document.getElementById('toolkit');
+      if(selectedText){
+      const range = window.getSelection().getRangeAt(0);
+      const rect = range.getBoundingClientRect();
+      toolkit.style.left = `${rect.left + window.scrollX}px`;
+      toolkit.style.top = `${rect.bottom + window.scrollY}px`;
+      toolkit.classList.remove('hidden');
+      }
+      else {
+        toolkit.classList.add('hidden');
+      }
+  }
+   
  
   let [text,setText]= useState("")
   const toUpper=()=>{
@@ -38,24 +58,24 @@ function Home(props) {
       navigator.clipboard.writeText(text)
       alert("Text copied to clipboard!")
     }
-   
   }
-  const textarea = document.getElementById('myTextarea');
-  document.addEventListener('mouseup', function() {
-    if(textarea.value!=null){
-      const selectedText = textarea.value.substring(textarea.selectionStart,textarea.selectionEnd);
-      console.log(selectedText);
-    }
-  
-  });
-  
-  
+  const copy=()=>{
+    navigator.clipboard.writeText(selectedText)
+      alert("Text copied to clipboard!")
+  }
     return(
    <>
+   <div id="toolkit" className="toolkit hidden">
+      <button className='btn' onClick={toUpper}>Upper</button>
+      <button className='btn' onClick={toLower}>Lower Case</button>
+      <button className='btn' onClick={cleartxt}>Clear</button>
+      <button className='extra' onClick={extraSpaces}>Remove Extra Spaces</button>
+      <button className='btn' onClick={cpyText}>Copy Text</button>
+      </div>
       <div className='home' style={props.color}>
       <h1>Enter the text below</h1>
       <div className='txtarea'>
-      <textarea style={props.mycolor} placeholder='Enter any text' value={text} onChange={handleChange} rows={8} ></textarea>
+      <textarea id='myTextarea' style={props.mycolor} placeholder='Enter any text' value={text} onChange={handleChange} rows={8} onMouseUp={handleMouseUp} ></textarea>
       </div>
       <br/>
       <div className='btns'>
@@ -75,7 +95,6 @@ function Home(props) {
         <p>{((text.split(/\s+/).filter((element)=>{return element.length!==0})).length)*0.4 } Seconds</p>
       </div>
       <br/>
-    
       </div>  
    </>
     )
